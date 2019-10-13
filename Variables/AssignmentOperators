4f0525b0ae34cb8985500ec87e1b01342c8a8031
ashwinipardeshi1655 <53752603+ashwinipardeshi1655@users.noreply.github.com>
2019-10-13 14:41:15 +0530

    Variables
    Core Java

---
+++ Variables/AssignmentOperators
@@ -0,0 +1,9 @@
+public class AssignmentOperators{
+	public static void main(String[]args){
+		int i ;
+		int j ;
+		i = 10 ;
+		j = 0 ;
+		System.out.println("i = " +i);
+		System.out.println("j = "  +j);
+}}
---
+++ Variables/Variables.java
@@ -0,0 +1,37 @@
+public class Variables{
+	public static void main(String[]args){
+		int i ;                             //initialization
+		i = 10 ;                            //declaration 
+		System.out.println(i) ;             //utilization
+		
+		
+		byte b ;
+		b = 23 ;
+		System.out.println(b);
+		
+		short s ;
+		s = 123 ;
+		System.out.println(s);
+		
+		long l ;
+		l = 23456;
+		System.out.println(l);
+		
+		double d ;
+		d = 235886 ; 
+		System.out.println(d);
+		
+		float f ;
+		f = 1.12345678f;
+		System.out.println(f) ;
+		
+		char c ; 
+		c = 'a' ;
+		System.out.println(c) ;
+		
+		boolean o ;
+		o = true ;
+		System.out.println(o) ;
+		
+	}
+}
